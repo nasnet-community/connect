1025f9ae1168b8a84060bb9f746c65144ee79864
Mark Pashmfouroush <mark@markpash.me>
2025-02-28 09:35:53 +0000

    feat: add TypeScript definitions for environment variables and image modules

---
+++ src/types/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    readonly VITE_APP_VERSION: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+  
+  declare module "*.jpg" {
+    const content: string
+    export default content
+  }
+  
+  declare module "*.gif" {
+    const content: string
+    export default content
+  }
