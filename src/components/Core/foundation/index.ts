f558ea8468ca596e6dfe471169f3307c21a7be0c
Mark Pashmfouroush <mark@markpash.me>
2026-03-23 09:10:34 +0000

    delete more useless crap

--- src/components/Core/foundation/index.ts
+++ src/components/Core/foundation/index.ts
@@ -1,28 +1,5 @@
 export { GlobalStyles } from "./GlobalStyles";
 export { ThemeToggle } from "./ThemeToggle";
 export type { Theme } from "./ThemeToggle";
-export { ColorTokensVisual, ColorSample } from "./ColorTokensVisual";
-export {
-  TypographyTokensVisual,
-  TypographySample,
-  FontWeightSample,
-  LineHeightSample,
-} from "./TypographyTokensVisual";
-export {
-  SpacingTokensVisual,
-  SpacingSample,
-  SpacingPreset,
-  SpacingUsageExample,
-} from "./SpacingTokensVisual";
-export {
-  EffectTokensVisual,
-  ShadowSample,
-  BorderRadiusSample,
-  AnimationSample,
-  TransitionSample,
-} from "./EffectTokensVisual";
 export { RTLProvider } from "./RTLProvider";
 export type { RTLDirection, RTLLanguage } from "./RTLProvider";
-export { ThemeTest } from "./ThemeTest";
-export { ThemeTransitionTest } from "./ThemeTransitionTest";
-export { ThemePersistenceTest } from "./ThemePersistenceTest";
